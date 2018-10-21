6fd87deeaa4262939f6da2a2b9603c0acca81f69
Erik <andreazi.erik@gmail.com>
2018-10-21 19:07:33 -0300

    Correção do retorno JSON

--- src/pages/perfil/perfil.ts
+++ src/pages/perfil/perfil.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { ApiProvider } from '../../providers/api/api';
 import { Observable } from 'rxjs/Observable';
 
+
 @Component({
   selector: 'page-perfil',
   templateUrl: 'perfil.html',
@@ -15,7 +16,7 @@ export class PerfilPage {
 
   public perfil = new Array<any>();
 
-  constructor(public navCtrl: NavController, public http: HttpClient, private API: ApiProvider) {
+  constructor(public navCtrl: NavController, public http: HttpClient) {
     this.getPerfilGithub();
   }
 
@@ -24,25 +25,7 @@ export class PerfilPage {
     let data: Observable<any> = this.http.get(url);
     data.subscribe( result => {
       this.perfil = result;
-      console.log(result);
     });
   }
-
-  /*ionViewDidLoad(){
-    this.API.getPerfil().subscribe(
-      data => {
-
-        const response = (data as any);
-        const objeto_retorno = JSON.parse(response._body);
-        this.perfil = objeto_retorno.avatar_url;
-
-        console.log(objeto_retorno);
-      }, error => {
-        console.log(error);
-      }
-      
-    )
-    
-  }*/
 
 }
